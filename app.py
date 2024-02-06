a3b5875a928d5b4ddb693eee214e23dcfe1148fc
Desai Wang <dw584@cornell.edu>
2024-02-06 15:40:00 -0500

    host on landing page

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from whitenoise import WhiteNoise  # handles static files
 # app object is the web server, it's implicitly running without doing anything
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/",
-                          prefix="static/", index_file="index.htm", autorefresh=True)
+                          prefix="/", index_file="index.htm", autorefresh=True)
 
 
 # string indicates routes that need to be handled, here we just do root dir
-@app.route('/', methods=['GET'])
-def hello():
-    return make_response("please go to /static for hw1.")
+# @app.route('/', methods=['GET'])
+# def hello():
+#     return make_response("please go to static/ for hw1.")
 
 
 # have flask start server if calling from terminal
